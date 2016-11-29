29d33a46c687b2418d0b585a710faa838c197df3
you@example.com <you@example.com>
2016-11-29 17:25:38 +0200

    improved python telnet functionality (still works)

--- python_buffoonery/telnet.py
+++ python_buffoonery/telnet.py
@@ -6,7 +6,50 @@ PORT='20231'
 tn=telnetlib.Telnet(HOST, PORT)
 data=tn.read_until("Escape character is '^]'.", timeout=1)
 
+increment=3
+
+def goForward(distance):
+    print 'initiating goForward'
+    while distance>0:
+        tn.write("fs")
+        distance-=1
+        time.sleep(1)
+    print 'terminating goForward'
+
+def goBackward(distance):
+    while distance>0:
+        tn.write("bs")
+        distance-=1
+        time.sleep(1)
+
+def leftTurn(angle):
+    tn.write("l")
+    time.sleep(angle)
+    tn.write("s")
+
+def rightTurn(angle):
+    tn.write("r")
+    time.sleep(angle)
+    tn.write("s")
+
 n=10
+
+#leftTurn(0.7)
+#time.sleep(1)
+#leftTurn(0.7)
+#rightTurn(0.7)
+
+goForward(10)
+time.sleep(1)
+leftTurn(1.2)
+time.sleep(1)
+goForward(3)
+time.sleep(1)
+rightTurn(1.2)
+time.sleep(1)
+goBackward(7)
+time.sleep(10)
+
 while n>0:
     print 'sending ffs'
     tn.write("ffs")
